18cd57584be84d683ac90ea6814e9d50fc36980a
redstarbird <redstarbird32@gmail.com>
2022-08-20 14:52:55 +0200

    Started adding CLI interface to arrowpack

--- packages/ArrowPack/src/ArrowPack.js
+++ packages/ArrowPack/src/ArrowPack.js
@@ -1,8 +1,28 @@
 // js wrapper for arrowpack for NPM
 const fs = require("fs");
+const yargs = require("yargs");
+const chalk = require("chalk");
+const boxen = require("boxen");
 const wasm_exec = require("js/wasm_exec.js");
 const go = new wasm_exec.Go();
+import { version } from "./package.json";
 
-const wasmBuffer = fs.readFileSync("main.wasm");
-// do stuff eventually
-WebAssembly.instantiate(wasmBuffer, go.importObject).then((wasmModule) => {});
+if (process.argv[2]) {
+	if (
+		process.argv[2] == "--version" ||
+		process.argv[2] == "-version" ||
+		process.argv[2] == "-v" ||
+		process.argv[2] == "--v" ||
+		process.argv[2] == "version"
+	) {
+		console.log(version);
+	}
+} else if (process.argv[2] == "build") {
+	const wasmBuffer = fs.readFileSync("main.wasm");
+
+	WebAssembly.instantiate(wasmBuffer, go.importObject).then(
+		(wasmModule) => {}
+	);
+} else {
+	console.log('Please specify a command-line arg, such as "ArrowPack build"');
+}
