ce8dea462d402ee425ef7615144279486e210561
redstarbird <contact@redstarbird.com>
2022-10-27 22:06:47 +0100

    Added settingsSingleton.for

---
+++ packages/ArrowPack/src/SettingsSingleton/settingsSingleton.for
@@ -0,0 +1,1 @@
+! Fotran may used in future for very performance sensitive operations as it can sometimes be faster than C. It will need to be easier to compile to Wasm with the fast speed and small file size.
