9bf493b26ca763ed258c25d5ad9f691edc8b8516
redstarbird <contact@redstarbird.com>
2022-10-28 11:07:51 +0100

    Split fortran comment into 2 lines for readability

--- packages/ArrowPack/src/SettingsSingleton/settingsSingleton.for
+++ packages/ArrowPack/src/SettingsSingleton/settingsSingleton.for
@@ -1,1 +1,2 @@
-! Fotran may used in future for very performance sensitive operations as it can sometimes be faster than C. It will need to be easier to compile to Wasm with the fast speed and small file size.
+! Fotran may used in future for very performance sensitive operations as it can sometimes be faster than C. 
+! It will need to be easier to compile to Wasm with the fast speed and small file size.
