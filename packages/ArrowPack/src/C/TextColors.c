d9b4b88649c7abdbbfcba1a79ef54c07ed80171b
redstarbird <contact@redstarbird.com>
2022-11-09 11:14:29 +0000

    Created TextColors.c file for colored text

---
+++ packages/ArrowPack/src/C/TextColors.c
@@ -0,0 +1,13 @@
+#include "TextColors.h"
+
+void ColorRed() { printf("\x1b[31m"); }
+void ColorBlack() { printf("\x1b[30m"); }
+void ColorGreen() { printf("\x1b[32m"); }
+void ColorYellow() { printf("\x1b[33m"); }
+void ColorBlue() { printf("\x1b[34m"); }
+void ColorPink() { ColorMagenta(); }
+void ColorMagenta() { printf("\x1b[35m"); }
+void ColorCyan() { printf("\x1b[36m"); }
+void ColorWhite() { printf("\x1b[37m"); }
+void ColorReset() { ColorNormal(); }
+void ColorNormal() { printf("\x1b[m"); }
---
+++ packages/ArrowPack/src/C/TextColors.h
@@ -0,0 +1,18 @@
+#ifndef TEXTCOLORS_H
+#define TEXTCOLORS_H
+
+#include <stdio.h>
+
+void ColorRed();
+void ColorBlack();
+void ColorGreen();
+void ColorYellow();
+void ColorBlue();
+void ColorPink();
+void ColorMagenta();
+void ColorCyan();
+void ColorWhite();
+void ColorReset();
+void ColorNormal();
+
+#endif // !TEXTCOLORS_H
