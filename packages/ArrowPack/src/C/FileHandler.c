612ce5891d97819679153a4370c70f1e43dd2042
redstarbird <contact@redstarbird.com>
2022-11-10 15:43:58 +0000

    Created CreateFileWrite function for creating new files and writing data in C

--- packages/ArrowPack/src/C/FileHandler.c
+++ packages/ArrowPack/src/C/FileHandler.c
@@ -1,1 +1,23 @@
 #include "FileHandler.h"
+
+void CreateFileWrite(char *path, char *text)
+{
+    FILE *FilePTR;
+    FilePTR = fopen(path, "w");
+    if (!FilePTR)
+    {
+        printf("Error opening file %s\n", path);
+        exit(1);
+    }
+    if (strlen(text) > 0)
+    {
+        for (int i = 0; i < strlen(text); i++)
+        {
+            fputc(text[i], FilePTR);
+        }
+    }
+    fclose(FilePTR);
+}
+void CreateFile(char *path)
+{
+}
--- packages/ArrowPack/src/C/FileHandler.h
+++ packages/ArrowPack/src/C/FileHandler.h
@@ -2,5 +2,9 @@
 #define FILEHANDLER_H
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void CreateFileWrite(char *path, char *text);
 
 #endif // !FILEHANDLERH
