a5de132cb7463934fa3a43de110f4ca0874ae157
redstarbird <contact@redstarbird.com>
2022-12-04 10:17:32 +0000

    Removed ReadFile.h references in BundleFiles.h

--- packages/ArrowPack/src/C/BundleFiles.h
+++ packages/ArrowPack/src/C/BundleFiles.h
@@ -4,11 +4,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "StringRelatedFunctions.h"
-#include "ReadFile.h"
 #include "../DependencyTree/DependencyTree.h"
 #include "ProblemHandler.h"
-#include "ReadFile.h"
-#include "FileHandler.h"
+#include "./FileHandler.h"
 
 bool EMSCRIPTEN_KEEPALIVE BundleFiles(struct Node *DependencyTree);
 
