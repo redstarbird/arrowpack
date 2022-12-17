9f759d254d32ae126866f94f2d6a8d37b350a02e
redstarbird <contact@redstarbird.com>
2022-12-17 04:45:01 +0000

    Created CSS AST files

---
+++ src/AST/CSS_AST.c
@@ -0,0 +1,2 @@
+// CSS AST not needed yet
+#include "./CSS_AST.h"
---
+++ src/AST/CSS_AST.h
@@ -0,0 +1,8 @@
+// CSS AST not needed yet
+#ifndef CSS_AST_H
+#define CSS_AST_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif // !CSS_AST_H
