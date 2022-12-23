34a6ac64446a17cde9c83287fc1a511cfd392996
redstarbird <contact@redstarbird.com>
2022-12-23 20:04:11 +0100

    Modified struct definitions to work as a graph data structure

--- src/DependencyTree/DependencyTree.h
+++ src/DependencyTree/DependencyTree.h
@@ -13,24 +13,27 @@ typedef struct FileRule
     unsigned short int StartPos, EndPos; // endpos is pos from end of string
 } FileRule;
 
-typedef struct Node Node;
-
-typedef struct Dependency // Wraps a regular Node struct and includes the start and end positions of where the node is referenced so it doesn't need to be worked out again
+typedef struct Edge // Wraps a regular Node struct and includes the start and end positions of where the node is referenced so it doesn't need to be worked out again
 {
     unsigned int StartRefPos, EndRefPos;
-    char *DependencyPath;
-} Dependency;
+} Edge;
+
+typedef struct Node Node;
 
 struct Node
 { // structure for individual nodes (Modules/Files) in the tree
-    bool IsArrayEnd;
     char *path;
-    struct Node *Dependents;
-    struct Dependency *Dependencies;
+    bool visited;
     unsigned int DependenciesInTree, DependentsInTree;
+    struct Node *next;
     int FileType; // File type ID for file (File type IDs are define in FileTypesHandler.h)
 };
 
+typedef struct Graph
+{
+    int num_vertices; // Number of vertices in the graph
+    Node **adj_list;  // Array of pointers to the head of the linked lists for each vertex
+} Graph;
 struct Node *CreateTree(char *Wrapped_paths, int ArrayLength); // Creates dependency tree/graph/array
 
 char **FindDependencies(char *Path); // function that returns an array of strings representing dependencies for the given file
