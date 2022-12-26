c54d531a38faa7717915ca93662e2801848b34d0
redstarbird <contact@redstarbird.com>
2022-12-26 10:56:56 +0100

    Created functions for working with a stack data structure in C

---
+++ src/C/Stack.c
@@ -0,0 +1,65 @@
+#include "Stack.h"
+
+struct Stack *CreateStack(unsigned int Capacity, unsigned int type)
+{
+    size_t TypeSize = 0;
+
+    struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack));
+    stack->capacity = Capacity;
+    stack->top = -1;
+    switch (type)
+    {
+    case STACK_INT:
+        TypeSize = sizeof(int);
+        stack->array.IntArray = malloc(stack->capacity * TypeSize);
+        break;
+    case STACK_VERTEX:
+        TypeSize = sizeof(struct Node *);
+        stack->array.VertexArray = malloc(stack->capacity * TypeSize);
+        break;
+    }
+    stack->Type = type;
+    return stack;
+}
+
+bool StackIsFull(struct Stack *stack)
+{
+    return stack->top == stack->capacity - 1;
+}
+
+bool StackIsEmpty(struct Stack *stack)
+{
+    return stack->top == -1;
+}
+void StackpushV(struct Stack *stack, struct Node *Value)
+{
+    if (StackIsFull(stack))
+        return;
+    stack->array.VertexArray[++stack->top] = Value;
+    printf("%s pushed to stack\n", Value->path);
+}
+void StackpushI(struct Stack *stack, int Value)
+{
+    if (StackIsFull(stack))
+        return;
+    stack->array.IntArray[++stack->top] = Value;
+    printf("%d pushed to stack\n", Value);
+}
+int StackpopI(struct Stack *stack) // Need to make overload for this function
+{
+    if (StackIsEmpty(stack))
+    {
+        printf("Tried to remove top value from stack but stack is empty");
+    }
+
+    return stack->array.IntArray[stack->top--];
+}
+void StackpopV(struct Stack *stack) // Need to make overload for this function
+{
+    if (StackIsEmpty(stack))
+    {
+        printf("Tried to remove top value from stack but stack is empty");
+    }
+    stack->top--;
+    return;
+}
---
+++ src/C/Stack.h
@@ -0,0 +1,40 @@
+#ifndef STACK_H
+#define STACK_H
+#include <stdio.h>
+#include <stdbool.h>
+#include "../DependencyTree/DependencyTree.h"
+// A structure to represent a stack
+struct Stack
+{
+    unsigned int Type;
+    int top;
+    unsigned capacity;
+    union
+    {
+        int *IntArray;
+        struct Node **VertexArray;
+    } array;
+};
+
+enum
+{
+    STACK_INT,
+    STACK_CHAR,
+    STACK_VERTEX
+};
+
+void StackpushV(struct Stack *stack, struct Node *Value);
+void StackpushI(struct Stack *stack, int Value);
+void StackpopV(struct Stack *stack);
+int StackpopI(struct Stack *stack);
+bool StackIsEmpty(struct Stack *stack);
+bool StackIsFull(struct Stack *stack);
+struct Stack *CreateStack(unsigned int Capacity, unsigned int type);
+
+#define Stackpush(stack, Value) _Generic(Value, struct Node * \
+                                         : StackpushV, int    \
+                                         : StackpushI)(stack, Value) // Allows overloading of Stackpush function
+/*#define Stackpop(stack) _Generic(stack.array, struct Node * * \
+                                 : StackpopV, int *           \
+                                 : StackpopI)(stack) // Allows overloading of Stackpush function*/
+#endif // !STACK_H
