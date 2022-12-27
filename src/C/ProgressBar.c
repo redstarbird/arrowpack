077dd08a71fb6c4e576d5c559a1f911798fe5fe6
redstarbird <contact@redstarbird.com>
2022-12-27 09:40:54 +0100

    Created progress bar functions in C

---
+++ src/C/ProgressBar.c
@@ -0,0 +1,27 @@
+#include "ProgressBar.h"
+#define PROGRESS_BAR_LENGTH 50
+
+void print_progress_bar(int current, int total)
+{
+    const int bar_length = 50; // The number of '=' characters to use for the bar
+    int percentage = (int)current / total * 100;
+
+    printf("\r[");
+
+    int i;
+    printf("\033[s"); // Save the current cursor position
+    printf("[");
+    for (i = 0; i < PROGRESS_BAR_LENGTH; i++)
+    {
+        if (i < percentage / 2)
+        {
+            printf("=");
+        }
+        else
+        {
+            printf(" ");
+        }
+    }
+    printf("] %3d%%\033[u", percentage); // Restore the cursor position and clear the line
+    fflush(stdout);
+}
---
+++ src/C/ProgressBar.h
@@ -0,0 +1,8 @@
+#ifndef PROGRESSBAR_H
+#define PROGRESSBAR_H
+
+#include <stdio.h>
+
+void print_progress_bar(int current, int total);
+
+#endif // !PROGRESSBAR_H
