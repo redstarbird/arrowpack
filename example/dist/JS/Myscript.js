bea92b91fcc97d346be3a524ffb1f6e96693dfc5
Laurence Jackson <contact@redstarbird.com>
2026-08-20 18:26:50 +0100

    Update example dist build

--- example/dist/JS/Myscript.js
+++ example/dist/JS/Myscript.js
@@ -1,4 +1,4 @@
-function HelloWorldIfy(str) {    return "Hello World " + str;}let helloworldify_ARROWPACK = {};helloworldify_ARROWPACK = HelloWorldIfy;function ThisIsAModuleTestFunction() {    console.log("CommonJS module!");}let MyModulecjs_ARROWPACK = {};MyModulecjs_ARROWPACK.CJSModuleFunction = ThisIsAModuleTestFunction;const HelloWorldify = helloworldify_ARROWPACK;
+function HelloWorldIfy(str) {    return "Hello World " + str;}let helloworldify_ARROWPACK = {};helloworldify_ARROWPACK = HelloWorldIfy;function ThisIsAModuleTestFunction() {    console.log("CommonJS module!");}let MyModulecjs_ARROWPACK = {};MyModulecjs_ARROWPACK.CJSModuleFunction = ThisIsAModuleTestFunction;const HelloWorldify = helloworldify_ARROWPACK;
 const CJSModuleTest = MyModulecjs_ARROWPACK;
 
 CJSModuleTest.CJSModuleFunction();
